b079f0f1fbd320114823fd44aaf29d0f6b73c6ab
apsarabiswokarma <apsarabk94@gmail.com>
2024-01-03 22:53:49 +0545

    home page,jinja and register page

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template, request, redirect
 
+
 app = Flask(__name__)
 
 
+# @app.route("/")
+# def index():
+#     return render_template("pages/index.html")
+
+
 @app.route("/")
-def index():
-    return render_template("index.html")
+def hello_world():
+    return render_template("pages/home.html")
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("pages/login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("pages/register.html")
 
 
 @app.route("/dologin", methods=["POST"])
